cf24769a003a82ca472a53bbe75496c3f405ca16
Sanne V <36190479+mermaidlullaby@users.noreply.github.com>
2019-09-19 11:22:54 +0200

    Create script.js

---
+++ zombies/js/script.js
@@ -0,0 +1,11 @@
+alert("It is the zombie apocalypse. You are looting a store and suddenly a zombie bursts through the door!");
+var weapon = prompt("You search around frantically for a weapon. What do you choose?");
+var randomizer = Math.round(Math.random());
+
+alert("You attack the zombie with your " + weapon + ".");
+
+if (randomizer === 0){
+	alert("The zombie bites you. You lose!");
+} else if (randomizer === 1) {
+	alert("You kill the zombie with your " + weapon + ". You win!");	
+}
