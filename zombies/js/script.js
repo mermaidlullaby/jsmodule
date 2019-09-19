e461c50c0700235db37dd89c1ebf01c0c1e8a044
Sanne V <36190479+mermaidlullaby@users.noreply.github.com>
2019-09-19 13:10:49 +0200

    Add files via upload

--- zombies/js/script.js
+++ zombies/js/script.js
@@ -1,11 +1,11 @@
 alert("It is the zombie apocalypse. You are looting a store and suddenly a zombie bursts through the door!");
-var weapon = prompt("You search around frantically for a weapon. What do you choose?","axe");
+var weapon = prompt("You search around frantically for a weapon. What do you choose?", "axe");
 var randomizer = Math.round(Math.random());
 
 alert("You attack the zombie with your " + weapon + ".");
 
 if (randomizer === 0){
-	alert("The zombie bites you. You lose!");
+	document.getElementsByTagName("section")[0].innerHTML = "<p>You miss with your " + weapon + ". The zombie bites you. You lose!</p><p><a href=\"javascript:location.reload(true)\">Try again?</a></p>";
 } else if (randomizer === 1) {
-	alert("You kill the zombie with your " + weapon + ". You win!");	
+	document.getElementsByTagName("section")[0].innerHTML = "<p>The " + weapon + " successfully hits. You kill the zombie with your " + weapon + ". You win!</p><p><a href=\"javascript:location.reload(true)\">Try again?</a></p>";	
 }
